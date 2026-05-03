b954dd27ee5b4eaaad0280676e6b79203f8af70a
joao henrique da silva <joaohenriquedasilva244@gmail.com>
2026-05-03 01:25:18 -0300

    Prática 1 - Parte 2

---
+++ app/src/main/java/com/sistemaestudos/LoginActivity.kt
@@ -0,0 +1,96 @@
+package com.sistemaestudos
+
+import android.R.attr.enabled
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sistemaestudos.ui.theme.SistemaestudosTheme
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SistemaestudosTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginPage(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun LoginPage(modifier: Modifier = Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    val activity = LocalContext.current as Activity
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Bem-vindo/a!", fontSize = 24.sp)
+
+        Spacer(modifier = Modifier.size(12.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Digite seu e-mail") },
+            modifier = Modifier.fillMaxWidth(0.9f)
+        )
+
+        Spacer(modifier = Modifier.size(12.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Digite sua senha") },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth(0.9f)
+        )
+        Row(modifier = Modifier.padding(12.dp).fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            Button(
+                onClick = {
+                    Toast.makeText(activity, "Login OK!", Toast.LENGTH_LONG).show()
+                },
+                enabled = email.isNotEmpty() && password.isNotEmpty()
+            ) {
+                Text("Login")
+            }
+            Button(
+                onClick = { email = ""; password = "" }
+            ) {
+                Text("Limpar")
+            }
+        }
+    }
+}
