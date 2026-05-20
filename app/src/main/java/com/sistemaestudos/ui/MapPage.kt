9613d59a9b3b6f823cceb28ed4ab99888f2b0b16
João Henrique da Silva <joaohenriquedasilva244@gmail.com>
2026-05-20 10:25:59 -0300

    Prática 4 - Finalizada (Integração total com Mapas e Localização)
    Conclusão da Parte 4 do PDF. Adicionada a checagem nativa de permissões em tempo de execução no MapPage e ativados os recursos visuais de ponto azul (isMyLocationEnabled) e botão de foco de câmera (myLocationButtonEnabled) no GoogleMap.

--- app/src/main/java/com/sistemaestudos/ui/MapPage.kt
+++ app/src/main/java/com/sistemaestudos/ui/MapPage.kt
@@ -1,20 +1,28 @@
 package com.sistemaestudos.ui
 
+import android.content.pm.PackageManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
@@ -23,12 +31,24 @@ import com.sistemaestudos.MainViewModel
 @Composable
 fun MapPage(viewModel: MainViewModel, modifier: Modifier = Modifier) {
     val camPosState = rememberCameraPositionState ()
+
+    val context = LocalContext.current
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED
+        )
+    }
+
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = camPosState,
         onMapClick = { latLng ->
             viewModel.add("Cidade@${latLng.latitude}:${latLng.longitude}")
-        }
+        },
+        properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true)
     ) {
         viewModel.cities.forEach { city ->
             if (city.location != null) {
